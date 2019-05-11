6e648778fe0ad3d5ad175eb2461ec0dd3a7da55c
Adrian Toncean <adrianton3@gmail.com>
2019-05-11 09:32:16 +0200

    Add more tests

--- test/main.js
+++ test/main.js
@@ -17,28 +17,36 @@
 		return messages
 	}
 
-	[{
-		input: {
-			language: 'italian',
-			options: { count: 'max' },
-			startStrings: [''],
-			fudge: 6,
-			prefix: [0, 0],
-		},
-		expected: [{ type: 'solution', 'data': '0 0 0 0 0 0 3 3 0 0 7 0 7' }],
-	}].forEach(({ input, expected }) => {
+	function reflexicon (language, prefix, fudge, expected) {
+		return {
+			input: {
+				language: 'italian',
+				options: { count: 'max' },
+				startStrings: [''],
+				fudge,
+				prefix,
+			},
+			expected: [{ type: 'solution', data: expected }],
+		}
+	}
+
+	[
+		reflexicon('italian', [0, 0, 0], 6, '0 0 0 0 0 0 3 3 0 0 7 0 7'),
+		reflexicon('italian', [3, 4, 5], 6, '3 4 5 4 2 8 6 3 0 3 8 6 12'),
+		reflexicon('italian', [7, 5, 6], 6, '7 5 6 5 5 2 3 3 0 0 12 10 5'),
+	].forEach(({ input, expected }) => {
 		const actual = run(input)
 
 		const actualStr = JSON.stringify(actual)
 		const expectedStr = JSON.stringify(expected)
 
 		if (actualStr !== expectedStr) {
-			throw {
+			console.error({
 				message: 'Test failed',
 				input,
 				actual,
 				expected,
-			}
+			})
 		}
 	})
 })()
