eb92619f360eaf9a479a134410bd6b15cb61b898
Adrian Toncean <adrianton3@gmail.com>
2019-12-26 18:10:09 +0100

    Add french

---
+++ languages/french.js
@@ -0,0 +1,61 @@
+(() => {
+	'use strict'
+
+	const french = {
+		alphabet: 'abcdefghijklmnopqrstuvwxyz',
+		intros: [
+			'Cette phrase contient',
+		],
+		lastSeparators: [
+			'et',
+		],
+		numerals: [
+			'',
+			'un @',
+			'deux @',
+			'trois @',
+			'quatre @',
+			'cinq @',
+			'six @',
+			'sept @',
+			'huit @',
+			'neuf @',
+			'dix @',
+			'onze @',
+			'douze @',
+			'treize @',
+			'quatorze @',
+			'quinze @',
+			'seize @',
+			'dix-sept @',
+			'dix-huit @',
+			'dix-neuf @',
+			'vingt @',
+			'vingt et un @',
+			'vingt-deux @',
+			'vingt-trois @',
+			'vingt-quatre @',
+			'vingt-cinq @',
+			'vingt-six @',
+			'vingt-sept @',
+			'vingt-huit @',
+			'vingt-neuf @',
+			'trente @',
+			'trente et un @',
+			'trente-deux @',
+			'trente-trois @',
+			'trente-quatre @',
+			'trente-cinq @',
+			'trente-six @',
+			'trente-sept @',
+			'trente-huit @',
+			'trente-neuf @',
+			'quarante @',
+		],
+	}
+
+	auto.languages = auto.languages || {}
+	Object.assign(auto.languages, {
+		french,
+	})
+})()
